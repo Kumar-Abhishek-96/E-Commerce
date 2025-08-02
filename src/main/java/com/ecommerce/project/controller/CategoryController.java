029e804dd9b39a22f4a57dbe3d1ca9e11c5ebd0b
Kumar-Abhishek-96 <abhirajput1142@gmail.com>
2025-08-02 15:36:12 +0530

    Pagination

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
@@ -16,8 +17,16 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("public/categories")
-    public ResponseEntity<CategoryResponse> getAllCategories(){
-        CategoryResponse categoryResponse = categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(
+            @RequestParam(name = "pageNumber",defaultValue = AppConstants.DEFAULT_PAGE_NUMBER,required = false)
+            Integer pageNumber,
+            @RequestParam(name = "pageSize",defaultValue = AppConstants.DEFAULT_PAGE_SIZE,required = false)
+            Integer pageSize,
+            @RequestParam(name = "sortBy",defaultValue = AppConstants.DEFAULT_SORT_BY, required = false)
+            String sortBy,
+            @RequestParam(name = "sortOrder",defaultValue = AppConstants.DEFAULT_SORT_ORDER, required = false)
+            String sortOrder ){
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize,sortBy, sortOrder);
         return  new ResponseEntity<>(categoryResponse,HttpStatus.OK);
     }
 
