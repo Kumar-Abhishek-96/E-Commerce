d77c31c5b2196c3a51ec2d6aa7c2e84d7b6e4ccc
Kumar-Abhishek-96 <abhirajput1142@gmail.com>
2025-08-30 22:26:00 +0530

    Authentication Completed

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -1,17 +1,21 @@
 package com.ecommerce.project.security.jwt;
 
+import com.ecommerce.project.security.services.UserDetailsImp;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -25,19 +29,48 @@ public class JwtUtils {
     @Value("${spring.app.jwtSecret}")
     private String jwtSecret;
 
+    @Value("${spring.ecom.app.jwtCookieName}")
+    private String jwtCookie;
+
     // Generating JWT Form Header
-    public String getJwtFromHeaders(HttpServletRequest request){
-        String bearerToken = request.getHeader("Authorization");
-        logger.debug("Authorization Header: {}",bearerToken);
-        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
-            return bearerToken.substring(7);    // Remove bearer prefix --- Bearer abcd -- abcd
+//    public String getJwtFromHeaders(HttpServletRequest request){
+//        String bearerToken = request.getHeader("Authorization");
+//        logger.debug("Authorization Header: {}",bearerToken);
+//        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+//            return bearerToken.substring(7);    // Remove bearer prefix --- Bearer abcd -- abcd
+//        }
+//        return  null;
+//    }
+
+    public String getJwtFromCookies(HttpServletRequest request){
+        Cookie cookie = WebUtils.getCookie(request,jwtCookie);
+
+        if(cookie != null){
+            return cookie.getValue();
+        }else {
+            return null;
         }
-        return  null;
+    }
+
+    public ResponseCookie generateJwtCookie(UserDetailsImp userPrincipal){
+        String jwt = generateTokenFromUsername(userPrincipal.getUsername());
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie,jwt)
+                .path("/api")
+                .maxAge(24 * 60 * 60)
+                .httpOnly(false)
+                .build();
+        return cookie;
+    }
+
+    public ResponseCookie getCleanJwtCookie(){
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie,null)
+                .path("/api")
+                .build();
+        return cookie;
     }
 
     // Generating token from username
-    public String generateTokenFromUsername(UserDetails userDetails){
-        String username = userDetails.getUsername();
+    public String generateTokenFromUsername(String username){
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
